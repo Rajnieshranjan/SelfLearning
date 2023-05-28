63474fd4221acbad0a24cc81c5ebbf0ebb6eebe0
Rajnish Ranjan <rajnishr@interrait.com>
2023-05-28 14:20:30 +0530

    update class

---
+++ src/main/java/com/orangecrm/utility/Hi.java
@@ -0,0 +1,8 @@
+package com.orangecrm.utility;
+
+public class Hi {
+public static void main(String[] args) {
+	System.out.println("Hi Bro How are you ?");
+	System.out.println("I am good bro, how are you?");
+}
+}
